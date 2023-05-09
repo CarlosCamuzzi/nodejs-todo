a2a21cd1dc61267f6451970e1ba52243faac60da
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-05-08 22:02:51 -0300

    feature/bcrypt finish

--- backend/src/routes/login.route.js
+++ backend/src/routes/login.route.js
@@ -1,37 +1,39 @@
 const app = require("../../modules/express");
 const UserModel = require("../models/users/user.model");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // LOGIN
 app.post("/users/login", async (req, res) => {
   try {
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
+    const userEmailBody = req.body.email;
+    const userPasswordBody = req.body.password;
 
     const user = await UserModel.find({
-      email: userEmail,
-      password: userPassword,
+      email: userEmailBody,
+      //password: userPassword,
     });
-
-    // Fazer a comparação = Obs Refatorar cod
-    // bcrypt.compare(myPlaintextPassword, hash).then(function(result) {
-    //   // result == true
-    //});
 
     if (user[0] === undefined) {
       console.log("Unable to login, user not found");
-
       return res.status(404).json(user);
     } else {
-      console.log("Login ok, user found");
-      user.forEach((user) => {
-        const id = user.id;
-        const token = jwt.sign({ id }, process.env.SECRET, {
-          expiresIn: 6000,
-        });
+      // Compare password body and database
+      bcrypt.compare(userPasswordBody, user[0].password).then((result) => {
+        // console.log(result);
+        if (!result) {
+          return res.status(404).json({ Message: "Password Invalid" });
+        }
+        console.log("Login ok, user found");
+        user.forEach((user) => {
+          const id = user.id;
+          const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 6000,
+          });
 
-        console.log(token);
-        return res.status(200).json({ auth: true, token: token });
+          //console.log(token);
+          return res.status(200).json({ auth: true, token: token });
+        });
       });
     }
   } catch (error) {
